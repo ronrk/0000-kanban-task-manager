263e1f38df9c951d3fe8b3541daeaa3cbb3fdae0
ronrk <ronrk11@gmail.com>
2023-02-22 01:53:02 +0200

    feat: create new task and subtasks feature added

--- store/Api/taskApi.ts
+++ store/Api/taskApi.ts
@@ -4,51 +4,7 @@ export const taskApi = createApi({
   reducerPath: 'taskApi',
   tagTypes: ['tasks'],
   baseQuery: fetchBaseQuery({ baseUrl: '/api/tasks' }),
-  endpoints: (builder) => ({
-    getTask: builder.query({
-      query: (taskId: string) => `/${taskId}`,
-      providesTags: ['tasks'],
-    }),
-    createNewTask: builder.mutation({
-      query: (task) => ({
-        url: '',
-        method: 'POST',
-        body: task,
-      }),
-      invalidatesTags: ['tasks'],
-    }),
-    createNewSubtask: builder.mutation({
-      query: ({ taskId, subtask }: { taskId: string; subtask: any }) => ({
-        url: `/${taskId}`,
-        method: 'POST',
-        body: subtask,
-      }),
-      invalidatesTags: ['tasks'],
-    }),
-    removeTask: builder.mutation({
-      query: (taskId) => ({
-        url: `/${taskId}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['tasks'],
-    }),
-    removeSubtask: builder.mutation({
-      query: ({
-        taskId,
-        subtaskId,
-      }: {
-        taskId: string;
-        subtaskId: string;
-      }) => ({ url: `/${taskId}/${subtaskId}`, method: 'DELETE' }),
-      invalidatesTags: ['tasks'],
-    }),
-  }),
+  endpoints: (builder) => ({}),
 });
 
-export const {
-  useGetTaskQuery,
-  useCreateNewTaskMutation,
-  useCreateNewSubtaskMutation,
-  useRemoveTaskMutation,
-  useRemoveSubtaskMutation,
-} = taskApi;
+export const {} = taskApi;
